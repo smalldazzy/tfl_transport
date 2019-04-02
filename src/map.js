42ee23c5d6305747e90bb222bbb98a2082e427d6
smalldazzy <oleg.nod@gmail.com>
2019-04-03 01:40:26 +0300

    remake map

--- src/map.js
+++ src/map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+//import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import Provider from './context.js'
 
 const G_API='AIzaSyDuLQPPCI1hxAFfPfV0bs4XKn0wlwBGMQw';
@@ -25,7 +25,50 @@ export class MapContainer extends Component {
   //       });
   //     }
   // }
+  componentDidMount(){
+    this.renderMap();
+  }
+  renderMap = () => {
+    loadScript(`https://maps.googleapis.com/maps/api/js?key=${G_API}&callback=initMap`)
+    window.initMap = this.initMap
+  }
 
+
+  initMap = () => {
+   // Create A Map
+   var map = new window.google.maps.Map(document.getElementById('map'), {
+     center: {lat: -34.397, lng: 150.644},
+     zoom: 8
+   })
+ }
+
+   // Create An InfoWindow
+ //   var infowindow = new window.google.maps.InfoWindow()
+ //
+ //   // Display Dynamic Markers
+ //   this.state.venues.map(myVenue => {
+ //
+ //     var contentString = `${myVenue.venue.name}`
+ //
+ //     // Create A Marker
+ //     var marker = new window.google.maps.Marker({
+ //       position: {lat: myVenue.venue.location.lat , lng: myVenue.venue.location.lng},
+ //       map: map,
+ //       title: myVenue.venue.name
+ //     })
+ //
+ //     // Click on A Marker!
+ //     marker.addListener('click', function() {
+ //
+ //       // Change the content
+ //       infowindow.setContent(contentString)
+ //
+ //       // Open An InfoWindow
+ //       infowindow.open(map, marker)
+ //     })
+ //
+ //   })
+ // }
 
 
 
@@ -34,20 +77,30 @@ export class MapContainer extends Component {
     if (!this.props.store.loadingStations) console.log(this.props.store.stations[1].lat);
     console.log('tic');
     return (
-      <Map
-        google={this.props.google}
-        zoom={10}
-        style={mapStyles}
-        initialCenter={{
-          lat: 51.519105,
-          lng: -0.128405
-        }}><Marker title = { 'Changing Colors Garage' }
-          position = {{ lat: 51.505068 , lng: -0.126524 }}/>
-        </Map>
+      <div id='map'></div>
+      // <Map
+      //   google={this.props.google}
+      //   zoom={10}
+      //   style={mapStyles}
+      //   initialCenter={{
+      //     lat: 51.519105,
+      //     lng: -0.128405
+      //   }}><Marker title = { 'Changing Colors Garage' }
+      //     position = {{ lat: 51.505068 , lng: -0.126524 }}/>
+      //   </Map>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: G_API
-})(MapContainer)
+function loadScript(url) {
+  var index  = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+export default MapContainer;
+// export default GoogleApiWrapper({
+//   apiKey: G_API
+// })(MapContainer)
